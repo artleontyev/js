1e483ffa821d5a319a523c5195de395f78860c10
Artyom <lag.win@yandex.ru>
2019-04-19 21:45:15 +0300

    Изменения в дз №1

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 'use strict';
 
-let money,
-    time;
-
-money = prompt ("Ваш бюджет на месяц?", '');
-time = prompt ("Введите дату в формате YYYY-MM-DD", '');
+let money = prompt("Ваш бюджет на месяц?", ''),
+  time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
   budget: money,
@@ -14,12 +11,13 @@ let appData = {
   savings: false
 };
 
-let exp1, 
-    exp2;
-exp1= prompt ("Введите обязательную статью расходов в этом месяце", '');
-appData.expenses[exp1] = prompt ("Во сколько обойдется?", '');
+let exp1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    exp2 = prompt("Во сколько обойдется?", ''),
+    exp3 = prompt("Введите вторую обязательную статью расходов в этом месяце", ''),
+    exp4 = prompt("Во сколько обойдется?", '');
 
-exp2 = prompt("Введите вторую обязательную статью расходов в этом месяце", '');
-appData.expenses[exp2] = prompt("Во сколько обойдется?", '');
+appData.expenses.exp1 = exp2;
+appData.expenses.exp3 = exp4;
 
-alert (`Вам понадобится ${appData.expenses[exp1]/30} рублей в день для ${exp1}\n и ${appData.expenses[exp2]/30} рублей в день для ${exp2}`);
+
+alert(` Вам понадобится ${appData.budget/30} рублей в день `);
